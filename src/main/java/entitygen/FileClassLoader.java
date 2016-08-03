0546ab581677975f491307f418abec0cc3343e13
Ian <ian@darwinsys.com>
2016-08-03 16:29:34 -0400

    Make it grow up to be a real ClassLoader

--- src/main/java/entitygen/FileClassLoader.java
+++ src/main/java/entitygen/FileClassLoader.java
@@ -1,18 +1,35 @@
 package entitygen;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 
+/**
+ * Plain File-based class loader
+ * @author Ian Darwin
+ */
 public class FileClassLoader extends ClassLoader {
 
-	public FileClassLoader() {
-		// Empty
+	private final String dir;
+
+	public FileClassLoader(String dir) {
+		this.dir = dir;
+	}
+	
+	@Override
+	public Class<?> loadClass(String className) throws ClassNotFoundException {
+		String fileName = dir + "/" + className.replaceAll("\\.", "/") + ".class";
+		if (new File(fileName).exists()) {
+			return nameToClass(className, fileName);
+		} else {
+			return getSystemClassLoader().loadClass(className);
+		}
 	}
 
 	public Class<?> nameToClass(String className, String fileName) {
-		try (InputStream is = new FileInputStream(fileName);) {
+		try (InputStream is = new FileInputStream(fileName)) {
 			int n = is.available();
 			byte[] bytes = new byte[n];
 			int ret = is.read(bytes, 0, n);
